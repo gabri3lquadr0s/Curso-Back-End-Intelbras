bef2c77a86200113d5d677859c55bc7a84518d29
gabri3elquadr0s <gabrielquadrosrodapiao@gmail.com>
2023-03-02 17:08:15 -0300

    array

---
+++ Lógica de Programação/JavaScript/exemploArray.js
@@ -0,0 +1,6 @@
+var valores = [`Mario`,`Junior`,`Marcos`,`Ademar`,`Fabio`]
+console.log(valores[1], valores[4])
+console.log(valores)
+console.log(valores.length)
+valores.push(`Renan`,`Otavio`, 7, 8, true)
+//melhor deixar todos os valores dentro de uma array de um só tipo
