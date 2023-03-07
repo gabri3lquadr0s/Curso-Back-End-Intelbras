c785bf42ad0ae5a25df51b0ca65be44939dc959a
gabri3elquadr0s <gabrielquadrosrodapiao@gmail.com>
2023-03-07 17:15:31 -0300

    Add files via upload

---
+++ Lógica de Programação/JavaScript/Calculador de horas.js
@@ -0,0 +1,45 @@
+var hora1
+var minuto1
+var hora2
+var minuto2
+
+function valor1(){
+    hora1 = prompt(`Digite a hora`)
+    minuto1 = prompt(`Digite o minuto`)
+    if(hora1 > 12 || hora1 < 1 || minuto1 > 59 || minuto1 < 1){
+        alert(`Valor invalido`)
+        valor1()
+    }
+}
+function valor2(){
+    hora2 = prompt(`Digite a hora`)
+    minuto2 = prompt(`Digite o minuto`)
+    if(hora2 > 12 || hora2 < 1 || minuto2 > 59 || minuto2 < 1){
+        alert(`Valor invalido`)
+        valor2()
+    }
+}
+
+function conta(){
+    var opcao = prompt(`1-Soma\n2-Subtração`)
+    switch(opcao){
+        case `1`:
+            var resultadoHora = Number(hora1) + Number(hora2)
+            var resultadoMinuto = Number(minuto1) + Number(minuto2)
+            while(resultadoMinuto > 59){
+                resultadoMinuto-=60
+                resultadoHora+=1
+            }
+            alert(`O horário é ${resultadoHora}:${resultadoMinuto}`)
+            break;
+        case `2`:
+            var resultadoHora = hora1 - hora2
+            var resultadoMinuto = minuto1 - minuto2
+            alert(`O horário é ${resultadoHora}:${resultadoMinuto}`) 
+            break;
+    }
+}
+
+valor1()
+valor2()
+conta()
