49fa45757cf841dad4b0dc37a4e7f39452d59d92
gabri3elquadr0s <gabrielquadrosrodapiao@gmail.com>
2023-03-08 08:45:15 -0300

    Minutes are  correct now

--- Lógica de Programação/JavaScript/Calculador de horas.js
+++ Lógica de Programação/JavaScript/Calculador de horas.js
@@ -6,7 +6,7 @@ var minuto2
 function valor1(){
     hora1 = prompt(`Digite a hora`)
     minuto1 = prompt(`Digite o minuto`)
-    if(hora1 > 12 || hora1 < 1 || minuto1 > 59 || minuto1 < 1){
+    if(hora1 > 12 || hora1 < 1 || minuto1 > 59 || minuto1 < 0){
         alert(`Valor invalido`)
         valor1()
     }
@@ -14,7 +14,7 @@ function valor1(){
 function valor2(){
     hora2 = prompt(`Digite a hora`)
     minuto2 = prompt(`Digite o minuto`)
-    if(hora2 > 12 || hora2 < 1 || minuto2 > 59 || minuto2 < 1){
+    if(hora2 > 12 || hora2 < 1 || minuto2 > 59 || minuto2 < 0){
         alert(`Valor invalido`)
         valor2()
     }
@@ -35,7 +35,7 @@ function conta(){
         case `2`:
             var resultadoHora = hora1 - hora2
             var resultadoMinuto = minuto1 - minuto2
-            alert(`O horário é ${resultadoHora}:${resultadoMinuto}`) 
+            alert(`O horário é 0${resultadoHora}:0${resultadoMinuto}`) 
             break;
     }
 }
