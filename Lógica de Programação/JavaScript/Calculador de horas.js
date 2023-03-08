ec8aebae2ce75a67e67c23a40d19eec04d31b08d
gabri3elquadr0s <gabrielquadrosrodapiao@gmail.com>
2023-03-08 13:55:34 -0300

    eliminate unnecessary function

--- Lógica de Programação/JavaScript/Calculador de horas.js
+++ Lógica de Programação/JavaScript/Calculador de horas.js
@@ -3,15 +3,13 @@ var minuto1
 var hora2
 var minuto2
 
-function valor1(){
+function valores(){
     hora1 = prompt(`Digite a hora`)
     minuto1 = prompt(`Digite o minuto`)
     if(hora1 > 12 || hora1 < 1 || minuto1 > 59 || minuto1 < 0){
         alert(`Valor invalido`)
         valor1()
     }
-}
-function valor2(){
     hora2 = prompt(`Digite a hora`)
     minuto2 = prompt(`Digite o minuto`)
     if(hora2 > 12 || hora2 < 1 || minuto2 > 59 || minuto2 < 0){
@@ -30,7 +28,7 @@ function conta(){
                 resultadoMinuto-=60
                 resultadoHora+=1
             }
-            alert(`O horário é ${resultadoHora}:${resultadoMinuto}`)
+            alert(`O horário é ${resultadoHora} horas e ${resultadoMinuto} minutos.`)
             break;
         case `2`:
             var resultadoHora = hora1 - hora2
@@ -38,11 +36,10 @@ function conta(){
             if(resultadoMinuto < 0){
                 resultadoMinuto = resultadoMinuto * -1
             }
-            alert(`O horário é ${resultadoHora} horas e ${resultadoMinuto} minutos.`)  
+            alert(`O horário é ${resultadoHora} horas e ${resultadoMinuto} minutos.`) 
             break;
     }
 }
 
-valor1()
-valor2()
+valores()
 conta()
